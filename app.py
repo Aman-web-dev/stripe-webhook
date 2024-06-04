b347ff4f114b28fdfd0ef68ee25e8eeeb768586f
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-06-05 00:11:24 +0530

    payload data  changed

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ endpoint_secret = 'we_1PO1vh01Pe4bfmvIBs3NF5VX'  # Replace with your actual endp
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    payload = request.get_data(as_text=True)
+    # payload = request.get_data(as_text=True)
+    payload = request.data
     sig_header = request.headers.get('STRIPE_SIGNATURE')
     event = None
 
